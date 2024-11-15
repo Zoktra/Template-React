f0e9269e6d6fc92d7552993d69f589e9550e6543
Angel Gallego <angelgb1990@gmail.com>
2024-11-15 19:32:19 +0100

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+      <div>
+        <h1 className='text-warning'>Hola mundo</h1>
+      </div>
+
+  )
+}
+
+export default App
